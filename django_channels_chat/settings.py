91ad34d413a0edaa09abe21e170c69fde6169a65
Sergey <main0serg@gmail.com>
2024-10-20 13:10:38 +0600

    added custom template for auth user

--- django_channels_chat/settings.py
+++ django_channels_chat/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = "django_channels_chat.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -89,3 +89,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
